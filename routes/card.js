4ae2b7dd77154bfb75d90b8a4937a5f90f28c4f4
Your Name <you@example.com>
2020-04-11 19:44:08 +0300

    fix csrf for card delete

--- routes/card.js
+++ routes/card.js
@@ -5,10 +5,10 @@ const auth = require('../middleware/auth')
 
 function mapCartItems(cart) {
     return cart.items.map(c => ({
-         ...c.courseId._doc,
-         id:c.courseId.id, 
-         count: c.count 
-        }))
+        ...c.courseId._doc,
+        id: c.courseId.id,
+        count: c.count
+    }))
 }
 
 function computePrise(courses) {
@@ -17,25 +17,25 @@ function computePrise(courses) {
     }, 0)
 }
 
-router.post('/add',auth, async (req, res) => {
+router.post('/add', auth, async (req, res) => {
     const course = await Course.findById(req.body.id)
     await req.user.addToCart(course)
 
     res.redirect('/card')
 })
 
-router.delete('/remove/:id', auth,async (req, res) => {
+router.delete('/remove/:id', auth, async (req, res) => {
     await req.user.removeFromCart(req.params.id)
     const user = await req.user.populate('cart.items.courseId').execPopulate()
 
     const courses = mapCartItems(user.cart)
     const cart = {
-        courses, price:computePrise(courses)
+        courses, price: computePrise(courses)
     }
     res.status(200).json(cart)
 })
 
-router.get('/', auth,async (req, res) => {
+router.get('/', auth, async (req, res) => {
     const user = await req.user
         .populate('cart.items.courseId')
         .execPopulate()
